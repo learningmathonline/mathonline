15809f7a9cf9d0d7a7000d5357c500f505349d44
learningmathonline <113631440+learningmathonline@users.noreply.github.com>
2022-09-16 09:42:09 -0500

    Update applicationKeys.js

--- applicationKeys.js
+++ applicationKeys.js
@@ -5,6 +5,12 @@ const dict = {
     '343hj545h45nb5': 1,
     '5th4jth545tjhf': 1,
     'hrfjfh45b4rbf5': 1,
+    'ejfrh454hf4u5f': 1,
+    '%^%GHBhjbjhbfr': 1,
+    'HGYT6t74b%^hhj': 1,
+    'ghgh%$tfftGHGs': 1,
+    'HJ67%RvgvGHFvg': 1,
+    'GTF$^nhggvfxgG': 1,
 }
 
 export default dict;
