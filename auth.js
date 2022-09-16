d85a7fbc67884b6a48beb7f2cac29775656564b2
learningmathonline <113631440+learningmathonline@users.noreply.github.com>
2022-09-15 22:04:34 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,8 +3,9 @@ import CLIENT_ID from "./clientid.js";
 var x = prompt("Enter Application key")
 if (dict[x]) {
     dict[x] = 0;
+    alert('All Access Codes Expire After A Month Of Use')
 } else {
     body = document.getElementsByTagName('body')[0].innerHTML = `
-        <h1>Please Gain Access Code For Amazing Website</p>
+        <h1>That Was Eatheir A Wrong Or Expired Code, To Gain Access To This Website Please Gain A Access Code</p>
     `;
 }
