ad1d41cdff503afee43b67476bf6635fc44f07ae
learningmathonline <113631440+learningmathonline@users.noreply.github.com>
2022-09-16 11:15:06 -0500

    Update applicationKeys.js

--- applicationKeys.js
+++ applicationKeys.js
@@ -1,16 +1,16 @@
 const dict = {
-    '45th4j5th45tbf': 1,
-    '343hj54543bnb5': 1,
-    'erhjfh54jh45hj': 1,
-    '343hj545h45nb5': 1,
-    '5th4jth545tjhf': 1,
-    'hrfjfh45b4rbf5': 1,
-    'ejfrh454hf4u5f': 1,
-    '%^%GHBhjbjhbfr': 1,
-    'HGYT6t74b%^hhj': 1,
-    'ghgh%$tfftGHGs': 1,
-    'HJ67%RvgvGHFvg': 1,
-    'GTF$^nhggvfxgG': 1,
+    '4ec3ca2e1e8e94d691133a6b5f71d875': 1,
+    '963f5356bfef54ef847a55d92f48ea2f': 1,
+    '09d4ba5b35bcaba8fed0f381112a1003': 1,
+    'db2ce76a01e447a38b6c0b210c3c75ee': 1,
+    '726039be702fe64947302be5eb26a733': 1,
+    '0448050c598529f85f7b37fa1b9e556f': 1,
+    '5010e1547023639ce3a72e017b35536a': 1,
+    '46165c7901c032beeefad29b7dd1e434': 1,
+    '6dc62a29dacc9316e9526254c8cded52': 1,
+    '999ef472f30c5e71bb71e9e775d0f766': 1,
+    '23ef746623d3b752fce9e37612ee0a84': 1,
+    'c4ddff2b38c80fae4ef7f21c958ff4ce': 1,
 }
 
 export default dict;
